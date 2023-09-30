b272e78ea3d2a9655ac9640f1ed151829b703cd1
Seth Steenken <sethsteenken@gmail.com>
2023-09-30 17:06:10 -0400

    Http pinger updates and fixes. added to compose

--- apps/http-pinger/entrypoint.sh
+++ apps/http-pinger/entrypoint.sh
@@ -1,6 +1,3 @@
-# Adding executable permissions
-chmod +x /entrypoint.sh /var/scripts/pinger.sh
-
 # Create a new crontab file
 touch /etc/cron.d/crontab
 
@@ -17,8 +14,5 @@ crontab /etc/cron.d/crontab
 # Starting the cron daemon. The & is important and will allow to execute scripts below
 cron -f &
 
-# You can execute any other script below this line
 bash /var/scripts/pinger.sh >> /var/log/pinger.log 2>&1
-
-# We'll tail logs 
 tail -f /var/log/pinger.log
